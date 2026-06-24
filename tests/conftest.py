edf1df74fb9de937ddf1f4fa671f59da64511054
Yahiro025 <bennettpayoyo3.14@gmail.com>
2026-06-24 13:54:58 +0800

    batch delete unwanted files and folders

--- tests/conftest.py
+++
@@ -1,5 +0,0 @@
-import os
-import sys
-
-# Add the project root to sys.path so that tests can import source modules
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
